7a3f545fbe55e3942f888aff76a9f95211439b9c
Selvakumar Kuppusamy <selvakumarkuppusamy@Selvakumars-MacBook-Pro.local>
2016-01-17 16:37:28 +0000

    javafiles

---
+++ helloworld1/appClientModule/helloworld1/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld1;
+
+//this class hard codes the display  message
+
+class HelloWorld
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello world");
+	}
+}
