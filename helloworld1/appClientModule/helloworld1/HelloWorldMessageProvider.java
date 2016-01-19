c39aeb965cb877119b4e6148e96b08a9aab7d8eb
Selvakumar Kuppusamy <selvakumarkuppusamy@Selvakumars-MBP.localdomain>
2016-01-19 20:17:15 +0000

    new java files-hello1 module

--- helloworld1/appClientModule/helloworld1/HelloWorldMessageProvider.java
+++ helloworld1/appClientModule/helloworld1/HelloWorldMessageProvider.java
@@ -1,2 +1,14 @@
 package helloworld1;
 
+public class HelloWorldMessageProvider {
+	//Message provider logic is separated
+	// from rest of the logic
+	
+
+	public String getMessage()
+	{
+		return "Hello world";
+	}
+	
+	
+}
