7f0288b441b6feb3a1c4413c8cbde18893971ec0
Selvakumar Kuppusamy <selvakumarkuppusamy@Selvakumars-MacBook-Pro.local>
2016-01-20 16:33:50 +0000

    Messagerenderer-interface

---
+++ helloworld1/appClientModule/helloworld1/MessageRenderer.java
@@ -0,0 +1,9 @@
+package helloworld1;
+
+public interface MessageRenderer 
+{
+public void renderer();
+public void setMessageprovider(MessageProvider provider);
+public MessageProvider getMessageProvider();
+
+}
