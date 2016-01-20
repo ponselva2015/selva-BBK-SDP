97e6cdefb8bb299e237d0d25385ccb035478f8a7
Selvakumar Kuppusamy <selvakumarkuppusamy@Selvakumars-MacBook-Pro.local>
2016-01-20 17:25:53 +0000

    java-with-Factory

---
+++ helloworld1/appClientModule/helloworld1/HelloWorldDecoupledWithFactory.java
@@ -0,0 +1,12 @@
+package helloworld1;
+
+public class HelloWorldDecoupledWithFactory {
+    public static void main(String[] args) {
+      MessageRenderer mr = MessageSupportFactory.getInstance()
+          .getMessageRenderer();
+      MessageProvider mp = MessageSupportFactory.getInstance()
+          .getMessageProvider();
+      mr.setMessageProvider(mp);
+      mr.render();
+} 
+    }
